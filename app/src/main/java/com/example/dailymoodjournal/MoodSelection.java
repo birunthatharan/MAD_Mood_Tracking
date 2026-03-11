7303531474c87915cd93c482cb4142ddce4c95ca
MSI <ict2022033@as.rjt.ac.lk>
2026-03-12 00:22:43 +0530

    Initial commit

--- app/src/main/java/com/example/dailymoodjournal/MoodSelection.java
+++ app/src/main/java/com/example/dailymoodjournal/MoodSelection.java
@@ -1,8 +1,6 @@
 package com.example.dailymoodjournal;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MoodHistory extends AppCompatActivity {
-
-    ListView listView;
-
-    String tutorials[]
-            ={"24th of March - Happy ", "25th of March - Happy ", "26th of March - Sad "};
-
+public class MoodSelection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_mood_history);
-        listView = findViewById(R.id.myListView);
-        ArrayAdapter<String> arr = new ArrayAdapter<String>(
-                this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,tutorials);
-        listView.setAdapter(arr);
-
+        setContentView(R.layout.activity_mood_selection);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Date), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
